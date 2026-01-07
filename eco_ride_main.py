e1a4f405bd15ea305d2893b144d984d70239922b
Aradhy2004 <gaurish865@gmail.com>
2026-01-07 16:38:00 +0530

    Main: welcome message entry point

---
+++ eco_ride_main.py
@@ -0,0 +1,12 @@
+class EcoRideMain:
+    """
+    Entry point for Eco-Ride Urban Mobility System
+    """
+
+    @staticmethod
+    def start():
+        print("Welcome to Eco-Ride Urban Mobility System")
+
+
+if __name__ == "__main__":
+    EcoRideMain.start()
